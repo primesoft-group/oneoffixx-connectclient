132026ea57985119b5d89b4a1bafc0ecb04ac99b
SeverinAmacher <severin.amacher@gmx.net>
2016-10-21 13:48:39 +0200

    bump version to 1.6

--- Windows/src/OneOffixx.ConnectClient.WinApp/Properties/AssemblyInfo.cs
+++ Windows/src/OneOffixx.ConnectClient.WinApp/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OneOffixx.ConnectClient.WinApp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("OneOffixx Connect Client")]
+[assembly: AssemblyDescription("Test Client for OneOffixx Connect")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sevitec Informatik AG")]
 [assembly: AssemblyProduct("OneOffixx.ConnectClient.WinApp")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Sevitec Informatik AG 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.7.0")]
-[assembly: AssemblyFileVersion("1.5.7.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
