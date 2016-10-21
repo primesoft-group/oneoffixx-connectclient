1e2bdbae580db8653f4736f73b6899bff8a3fa88
SeverinAmacher <severin.amacher@gmx.net>
2016-10-21 11:55:07 +0200

    xml parser added
    fix for  #3

---
+++ Windows/src/OneOffixx.ConnectClient.WinApp/XHelper/XmlNamespaceCollection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneOffixx.ConnectClient.WinApp.XHelper
+{
+    public class XmlNamespaceCollection : Collection<XmlNamespace>
+    {
+        public XmlNamespaceCollection()
+        {
+        }
+
+        public XmlNamespace[] ToArray()
+        {
+            List<XmlNamespace> namespaces = new List<XmlNamespace>(this);
+            return namespaces.ToArray();
+        }
+
+        public string GetNamespaceForPrefix(string prefix)
+        {
+            foreach (XmlNamespace ns in this)
+            {
+                if (ns.Prefix == prefix)
+                {
+                    return ns.Name;
+                }
+            }
+            return String.Empty;
+        }
+
+        public string GetPrefix(string namespaceToMatch)
+        {
+            foreach (XmlNamespace ns in this)
+            {
+                if (ns.Name == namespaceToMatch)
+                {
+                    return ns.Prefix;
+                }
+            }
+            return String.Empty;
+        }
+    }
+}
---
+++ Windows/src/OneOffixx.ConnectClient.WinApp/XHelper/XsdAttributeInformation.cs
@@ -0,0 +1,9 @@
+﻿
+namespace OneOffixx.ConnectClient.WinApp.XHelper
+{
+    public class XsdAttributeInformation
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+    }
+}
